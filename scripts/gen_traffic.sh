8ea2cb40bf80f7ff429ff7e25fda52bca8b19c53
Gillian Gunson <ggunson@github.com>
2018-04-23 08:59:48 -0700

    Update gen_traffic.sh

--- scripts/gen_traffic.sh
+++ scripts/gen_traffic.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 # starts the query threads in operations.sh
 
+KILL_FILE="/tmp/kill.txt"
+
+touch $KILL_FILE
+sleep 20
+rm -f $KILL_FILE
+
 echo "Starting selects"
 ./operations.sh select >> activity.log 2>&1 &
 sleep 0.1
@@ -9,10 +15,16 @@ echo "Starting inserts"
 sleep 0.3
 echo "Starting inserts"
 ./operations.sh insert >> activity.log 2>&1 &
-sleep 2
-echo "Starting updates"
-./operations.sh update >> activity.log 2>&1 &
-sleep 1
+sleep 0.5
+echo "Starting inserts"
+./operations.sh insert >> activity.log 2>&1 &
+sleep 0.5
+echo "Starting inserts"
+./operations.sh insert >> activity.log 2>&1 &
+sleep 0.5
+echo "Starting inserts"
+./operations.sh insert >> activity.log 2>&1 &
+sleep 0.5
 echo "Starting updates"
 ./operations.sh update >> activity.log 2>&1 &
 sleep 0.5
